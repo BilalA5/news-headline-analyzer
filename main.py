88251a2889c9f13f4ae2fadc73efcf7baa886fc6
bahmed721 <s.bilal.ahmed927@gmail.com>
2025-08-24 14:19:26 -0600

    Requests and Parsing

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
 import requests as rq
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import BeautifulSoup as bs
 
 url = input("Enter a valid URL")
 
@@ -10,3 +14,12 @@ while True:
 
 response = rq.get(url)
 html = response.text
+soup = bs.BeautifulSoup(html, 'html.parser')
+headlines = [headline.get_text() for headline in soup.find_all('h1')]
+
+if headlines:
+  print("Headlines found on the page:")
+  for idx, headline in enumerate(headlines, start=1):
+    print(f"{idx}: {headline}")
+else:
+  print("No headlines found on the page.")
